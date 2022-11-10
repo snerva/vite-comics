e1c750b886f7dfafac33257819f98ba299256ef4
snerva <selenenerva@live.it>
2022-11-10 20:49:26 +0100

    footer top menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import SiteHeader from './components/SiteHeader.vue';
 import SiteMain from './components/SiteMain.vue';
+import SiteFooter from './components/SiteFooter.vue';
 export default {
   name: 'App',
   components: {
     SiteHeader,
     SiteMain,
+    SiteFooter
   }
 }
 </script>
@@ -13,28 +15,7 @@ export default {
 <template>
   <SiteHeader></SiteHeader>
   <SiteMain></SiteMain>
-
-  <footer id="site_footer">
-    <div class="footer_top">
-      <div class="container">
-        <div class="footer_menu">
-          <div class="row">
-            <div class="col"></div>
-            <div class="col"></div>
-            <div class="col"></div>
-          </div>
-        </div>
-        <div class="background_logo"></div>
-      </div>
-    </div>
-
-    <div class="footer_bottom">
-      <div class="container">
-        <div class="sign_up"></div>
-        <div class="follow_us"></div>
-      </div>
-    </div>
-  </footer>
+  <SiteFooter></SiteFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,122 @@
+<script>
+export default {
+    name: 'SiteFooter'
+}
+</script>
+
+<template>
+    <footer id="site_footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="footer_menu d-flex align-items-center">
+                    <div class="row">
+                        <div class="col">
+                            <h2>dc comics</h2>
+                            <ul>
+                                <li>
+                                    <a href="">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="">Tv</a>
+                                </li>
+                                <li>
+                                    <a href="">Games</a>
+                                </li>
+                                <li>
+                                    <a href="">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="">News</a>
+                                </li>
+                            </ul>
+                            <h2>shop</h2>
+                            <ul>
+                                <li>
+                                    <a href="">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h2>dc</h2>
+                            <ul>
+                                <li>
+                                    <a href="">Terms Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="">Privacy policy (New)</a>
+                                </li>
+                                <li>
+                                    <a href="">Ad Choices</a>
+                                </li>
+                                <li>
+                                    <a href="">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="">Talent Workshops</a>
+                                </li>
+                                <li>
+                                    <a href="">CPSC Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h2>sites</h2>
+                            <ul>
+                                <li>
+                                    <a href="">DC</a>
+                                </li>
+                                <li>
+                                    <a href="">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="">DC Power Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer_bottom">
+            <div class="container">
+                <div class="sign_up"></div>
+                <div class="follow_us"></div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
