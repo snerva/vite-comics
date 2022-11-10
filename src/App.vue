1cffe59bcbc94713553d768b8394fd0a540b55e1
snerva <selenenerva@live.it>
2022-11-10 16:35:32 +0100

    fix scaffolding

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  <header id="site_header">
+    <div class="container"></div>
+  </header>
+  <main id="site_main">
+
+    <div class="main_contents">
+      <div class="container"></div>
+    </div>
+    <div class="menu_panel">
+      <div class="container"></div>
+    </div>
+
+  </main>
+  <footer id="site_footer">
+    <div class="footer_top">
+      <div class="container">
+        <div class="row">
+          <div class="col"></div>
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer_bottom">
+      <div class="container"></div>
+    </div>
+  </footer>
+</template>
+
+<style lang="">
+
+</style>
