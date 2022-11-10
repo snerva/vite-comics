c470ba9aa78c4c5d161a2db9d0462688e8676bbd
snerva <selenenerva@live.it>
2022-11-10 18:17:35 +0100

    navbar menu

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -6,7 +6,26 @@ export default {
 
 <template>
     <header id="site_header">
-        <div class="container"></div>
+        <div class="container">
+            <div class="navbar_header d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+
+                <div class="menu_header d-flex">
+                    <a href="">characters</a>
+                    <a class="active" href="">comics</a>
+                    <a href="">movies</a>
+                    <a href="">tv</a>
+                    <a href="">games</a>
+                    <a href="">collectibles</a>
+                    <a href="">videos</a>
+                    <a href="">fans</a>
+                    <a href="">news</a>
+                    <a href="">shop</a>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
