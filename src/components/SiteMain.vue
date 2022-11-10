936ef9b78fd37ce64fb920cbfb224a7b435439fd
snerva <selenenerva@live.it>
2022-11-10 21:27:47 +0100

    Bonus

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,6 +1,11 @@
 <script>
+import SiteMainBluePanel from './SiteMainBluePanel.vue';
 export default {
-    name: 'SiteMain'
+    name: 'SiteMain',
+    components: {
+        SiteMainBluePanel
+    }
+
 }
 </script>
 
@@ -12,30 +17,7 @@ export default {
 
             </div>
         </div>
-        <div class="menu_panel">
-            <div class="container d-flex justify-content-evenly align-items-center">
-                <div class="mp digital_comics">
-                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                    <a href="#">digital comics</a>
-                </div>
-                <div class="mp merchandise">
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                    <a href="#">dc merchandise</a>
-                </div>
-                <div class="mp subscription">
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                    <a href="#">subscription</a>
-                </div>
-                <div class="mp shop_locator">
-                    <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                    <a href="#">comic shop locator</a>
-                </div>
-                <div class="mp power_visa">
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                    <a href="#">dc power visa</a>
-                </div>
-            </div>
-        </div>
+        <SiteMainBluePanel></SiteMainBluePanel>
 
     </main>
 </template>
---
+++ src/components/SiteMainBluePanel.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'SiteMainBluePanel'
+}
+</script>
+
+<template>
+    <div class="menu_panel">
+        <div class="container d-flex justify-content-evenly align-items-center">
+            <div class="mp digital_comics">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">digital comics</a>
+            </div>
+            <div class="mp merchandise">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <a href="#">dc merchandise</a>
+            </div>
+            <div class="mp subscription">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <a href="#">subscription</a>
+            </div>
+            <div class="mp shop_locator">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <a href="#">comic shop locator</a>
+            </div>
+            <div class="mp power_visa">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <a href="#">dc power visa</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
