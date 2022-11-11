28b7fb02acee6040cf75d7f380a2501c1cc183c3
snerva <selenenerva@live.it>
2022-11-11 15:29:50 +0100

    banner background

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -13,8 +13,9 @@ export default {
     <main id="site_main">
 
         <div class="main_contents">
+            <div class="main_banner"></div>
             <div class="container">
-
+                <div class="comics"></div>
             </div>
         </div>
         <SiteMainBluePanel></SiteMainBluePanel>
